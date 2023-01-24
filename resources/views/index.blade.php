7a4f8471704fd5e854c46321b02203da38ca2dda
valyaA11en <54936873+valyaA11en@users.noreply.github.com>
2023-01-24 22:11:51 +0500

    проект

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,9 +5,39 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="/resources/css/app.css">
+    <title>Оргостим</title>
 </head>
 <body>
+<div class="wrapper">
+    <header class="header">
+        <div class="container">
+            <div class="header__wrap">
+                <img class="header__logo" src="/img/logo1.png" alt="logo">
+                <div class="header__presentation">
+                    <a class="header__presentation__btn" href="#">Наша презентация</a>
+                </div>
+                <div class="header__language">
+                    <img src="/img/russia-svgrepo-com.png" alt="russia" class="language__icon">
+                    <p class="language__text">Ru</p>
+                </div>
+            </div>
+            <div class="header__phone__block">
+                <div class="number__phone__card">
+                    <p class="number__phone__text">+7 (912) 777-54-45</p>
+                </div>
+            </div>
+        </div>
 
+    </header>
+    <main class="main">
+        <video class="main__background__video" autoplay muted loop id>
+            <source class="background__video" src="img/background-video.mp4" type="video/mp4"/>
+        </video>
+        <p></p>
+    </main>
+    <footer class="footer">2</footer>
+</div>
+<script src="resources/js/app.js"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+Route::get('/',[\App\Http\Controllers\Controller::class,'index'])->name('/');
+Route::post('/index',[\App\Http\Controllers\Controller::class,'index']);
