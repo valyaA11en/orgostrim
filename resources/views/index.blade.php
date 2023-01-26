ec1dddfaafa2ae55f7b66b6c871d833166b577e0
valyaA11en <54936873+valyaA11en@users.noreply.github.com>
2023-01-26 15:53:49 +0500

    проект

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,47 +6,70 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/resources/css/app.css">
+    <link rel="stylesheet" href="/resources/css/bootstrap-grid.css">
     <title>Оргостим</title>
 </head>
 <body>
 <div class="wrapper">
     <header class="header">
-        <div class="container">
+        <div class="header__wrapper container">
             <div class="header__logo__block">
-                <img class="header__logo" src="/resources/src/images/logo1.png" alt="logo">
+                <img class="header__logo" src="/resources/src/images/logo1.svg" alt="logo">
                 <div class="header__language">
-                    <img src="/resources/src/images/russia-svgrepo-com.png" alt="Ru">
+                    <img src="/resources/src/images/russia-svgrepo-com.svg" alt="Ru">
                     <a class="header__language__text" href="">Ru</a>
                 </div>
-                <div class="header__menu__block">
-                    <ul class="header__menu">
+            </div>
+            <div class="header__menu__block">
+                <ul class="header__menu">
+                    <a href="#">
                         <li class="header__menu__links">Продукция</li>
+                    </a>
+                    <a href="#">
                         <li class="header__menu__links">Партнерам</li>
+                    </a>
+                    <a href="#">
                         <li class="header__menu__links">Документация</li>
+                    </a>
+                    <a href="#">
                         <li class="header__menu__links">Контакты</li>
-                    </ul>
-                </div>
-                <div class="header__block__right">
-                    <div class="header__phone__number">
-                        <p class="phone__number__text">+7 (912) 777-54-45</p>
-                        <div class="phone__number__btn__block">
-                            <a class="phone__number__btn" href="">Заказать звонок</a>
-                        </div>
-
+                    </a>
+                </ul>
+            </div>
+            <div class="header__block__right">
+                <div class="header__phone__number">
+                    <p class="phone__number__text">+7 (912) 777-54-45</p>
+                    <div class="phone__number__btn__block">
+                        <a class="phone__number__btn" href="">Заказать звонок</a>
                     </div>
                 </div>
-                <div class="header__request">
-                    <a class="header__request__btn" href="">Оставить заявку</a>
-                </div>
-
             </div>
-
+            <div class="header__request">
+                <a class="header__request__btn" href="#">Оставить заявку</a>
+            </div>
         </div>
-
     </header>
     <main class="main">
-        2
+        <div class="intro">
+            <div class="main__intro">
+                <video class="intro__video" src="/resources/src/video/video.mov" autoplay muted></video>
+            </div>
+            <div class="intro__content">
+                <div class="container">
+                    <h1 class="intro__title">ОргоСтим</h1>
+                    <h3 class="intro__text">Богатый урожай - наша забота</h3>
+                    <p class="intro__description">Продукт для органического земледелия</p>
+                    <div class="intro__btn__block">
+                        <a class="intro__btn" href="#">Получить консультацию</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, beatae culpa ea eos excepturi facilis
+            fugit iusto magnam nesciunt nihil nisi obcaecati officiis, omnis possimus quae quis sequi ullam veniam?</p>
     </main>
+
     <footer class="footer">3</footer>
 </div>
 <script src="resources/js/app.js"></script>
